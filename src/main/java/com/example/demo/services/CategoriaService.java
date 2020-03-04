99a8a39e89d7b360835db44a2509707a5c1c3ad0
ElliotJames <joao_vr2011@hotmail.com>
2020-03-04 14:46:11 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -1,13 +1,12 @@
 package com.example.demo.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.domain.Categoria;
 import com.example.demo.repositories.CategoriaRepository;
-
-import javassist.tools.rmi.ObjectNotFoundException;
-
-import java.util.Optional;
 
 @Service
 public class CategoriaService {
